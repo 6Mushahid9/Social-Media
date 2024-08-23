28a5f30c05c092e35c305ddeab4420adfeb53d42
Mushahid Khisal Ansari <mush.study@gmail.com>
2024-08-23 20:13:26 +0530

    removed navigate from app &  version from index

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,4 @@
-import React, { version } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -19,9 +19,9 @@ import storage from 'redux-persist/lib/storage';
 import { PersistGate } from 'redux-persist/integration/react';
 
 const persistConfig = {key: "root", storage, version: 1}
-const persistReducer = persistReducer(persistConfig, authReducer)
+const persistedReducer = persistReducer(persistConfig, authReducer)
 const store = configureStore({
-  reducer: persistReducer,
+  reducer: persistedReducer,
   middleware: (getDefultMiddleware) =>{
     getDefultMiddleware({
       serializableCheck: [
